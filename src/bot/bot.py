243b0ad7679c5b966caef1b3089dfebe3b6ba456
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-03-01 13:09:23 +0300

    Updated bot creation

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -2,22 +2,13 @@ from aiogram import Bot, Dispatcher
 
 from src.bot.schedule import schedule_router
 from src.bot.start import start_router
-from src.config import settings
-
-TOKEN = settings.TG_BOT_TOKEN
 
 
-async def main():
-    bot = Bot(token=TOKEN)
+async def create_bot(token: str):
+    bot = Bot(token=token)
     dp = Dispatcher()
 
     dp.include_router(schedule_router)
     dp.include_router(start_router)
 
-    await dp.start_polling(bot)
-
-
-if __name__ == "__main__":
-    import asyncio
-
-    asyncio.run(main())
+    return bot, dp
