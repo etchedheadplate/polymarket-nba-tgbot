6641755f3ae647eb95be316f2b0ac81a2f92d352
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-03-03 00:08:41 +0300

    Updated schedule command

--- src/bot/schedule/handlers.py
+++ src/bot/schedule/handlers.py
@@ -3,23 +3,29 @@ from aiogram.filters import Command
 from aiogram.types import CallbackQuery, Message
 
 from src.bot.schedule.keyboards import get_schedule_keyboard
-from src.bot.schedule.text import ScheduleRange, ScheduleText
-from src.workers import get_games_schedule
+from src.bot.schedule.text import ScheduleRange
+from src.queue import RabbitMQConnection
+from src.tasks import Registry, ScheduleWorker
 
 router = Router()
 
 
 @router.message(Command("schedule"))
 async def schedule_command_handler(message: Message):
-    await message.answer("Выберите диапазон:", reply_markup=get_schedule_keyboard())
+    await message.answer("Select schedule range:", reply_markup=get_schedule_keyboard())
 
 
 @router.callback_query(F.data.in_([range.value for range in ScheduleRange]))
-async def handle_click(callback: CallbackQuery):
+async def handle_click(callback: CallbackQuery, connection: RabbitMQConnection, registry: Registry):
     if callback.message is None:
         return
 
     range_selected = ScheduleRange(callback.data)
-    await get_games_schedule(range_selected)
-    response_text = ScheduleText.RESPONSES[range_selected]
-    await callback.message.answer(response_text)
+    worker = ScheduleWorker(connection=connection, registry=registry)
+    payload = await worker.run(schedule=range_selected)
+    text_response: list[str] = []
+    for data in payload.values():
+        game = f"{data['date']}: {data['guest']} {data['host']}"
+        text_response.append(game)
+    formatted_text = f"{'\n'.join(text_response)}"
+    await callback.message.answer(formatted_text)
